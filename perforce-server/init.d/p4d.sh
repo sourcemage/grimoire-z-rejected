e837feeed7babce0d1336b6ae62d281be7564c21
Eric Sandall <eric@sandall.us>
2003-04-01 07:12:58 -0100

    * Added init.d install information

--- perforce-server/init.d/p4d.sh
+++ perforce-server/init.d/p4d.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# Source Mage init.d install information
+# SMGL-script-version=20030331
+# SMGL-START:3 4 5:S90
+# SMGL-STOP:0 1 2 6:K10
+#
 
 case $1 in
     start)  echo      "starting perforce server."
