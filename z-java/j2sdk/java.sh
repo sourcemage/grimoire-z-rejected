f5e1b3a57dc18aa6eb083ec1689b556207df925a
eric_schabell <>
2004-08-22 15:49:23 -0100

    Diverse fixes to get this working... am surprised that no one noticed it  was borked (old j2sdk-bin version for one thing). Now it is also a bit  dynamic in defining the java.sh JAVA_HOME links and handles the juggling of  j2sdk-bin version better.

--- z-java/j2sdk/java.sh
+++ z-java/j2sdk/java.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
-# First check if this variable is already set 
-# then if not set, check it (maybe), then set it 
+#
+JAVA_HOME=/usr/java/J2SDKDIR 
 
-if  [ -z "$JAVA_HOME" ] ; then 
-	JAVA_HOME=/usr/java/j2sdk 
-fi 
 export JAVA_HOME
 export PATH=$PATH:$JAVA_HOME/bin
