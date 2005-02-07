b5ff8657124e0ad53c875ec532bfae172d5e9212
David Michael Leo Brown Jr <dmlb2000@gmail.com>
2005-02-07 03:11:46 +0000

    Okay changed it back I think this explains how perforce finds the file in P4CONFIG hopefully this will help the next guy that comes along trying to figure out what the hell perforce is doing.

--- z-devel/perforce-client/p4.sh
+++ z-devel/perforce-client/p4.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
 # First check if this variable is already set 
-# then if not set, check it (maybe), then set it 
+# then if not set, check it (maybe), then set it
+
+# Okay the P4CONFIG variable is the location of the .p4config file
+# if you leave the variable as is perforce will look for the file
+# .p4config starting from the current directory and going down to
+# the root of your client.
+# If you give P4CONFIG an explicit location (ie ~/.p4config or
+# /root/.p4config) perforce will look there and only there for
+# the file.
 
 if  [ -z "$P4CONFIG" ] ; then 
-	P4CONFIG=~/.p4config  # For Perforce client 
+	P4CONFIG=.p4config  # For Perforce client 
 fi 
 export P4CONFIG
 
